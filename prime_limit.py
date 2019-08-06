553039e29c704205630afa26a97d3bf4d1e3722e
Pavithramariappan <53573708+Pavithramariappan@users.noreply.github.com>
2019-08-06 21:11:25 +0530

    Create prime_limit.py

---
+++ prime_limit.py
@@ -0,0 +1,12 @@
+lower = int(_input("Enter lower range: "))  
+upper = int(input("Enter upper range: "))  
+  
+for num in range(lower,upper + 1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num)  
+#This example will show the prime numbers between 10 and 50.
+
