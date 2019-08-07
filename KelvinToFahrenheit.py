209963152aefefaa643fc93b60eda96ecf3b79e7
Pavithramariappan <53573708+Pavithramariappan@users.noreply.github.com>
2019-08-07 19:43:24 +0530

    Create KelvinToFahrenheit.py

---
+++ KelvinToFahrenheit.py
@@ -0,0 +1,9 @@
+def KelvinToFahrenheit(Temperature):
+  assert(Temperature>=0)
+  return ((Temperature-273)*1.8)+32
+KelvinToFahrenheit(273)
+
+KelvinToFahrenheit(505.78)
+
+print (KelvinToFahrenheit(273))
+print (KelvinToFahrenheit(505.78))
